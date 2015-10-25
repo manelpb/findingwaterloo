ae5e1ffcd0bace52f80d64751ec0469b19b9c71d
Bobin Cherian <bobinbc@gmail.com>
2015-10-25 01:42:54 -0400

    Added leaflet-tilelayer-mask library. Made binding changes to restrict map to Waterloo region.

--- app/controllers/mapViewController.js
+++ app/controllers/mapViewController.js
@@ -1,40 +1,98 @@
 var map = '';
 var zoomLevel = 13;
 
-    var formHTML = "<form action=''><input type='text' value='test' /> <input type='button' /> </form>";
+var formHTML = "<form action=''><input type='text' value='test' /> <input type='button' /> </form>";
 
 'use strict';
 app.controller('mapViewController', ['$scope', function ($scope) {
-
     L.mapbox.accessToken = 'pk.eyJ1IjoiYm9iaW5iYyIsImEiOiJ2MkpyLTMwIn0.nay6_wKcFJpEC1y2Y6PDCw';
     //Waterloo coordindates - [43.4822754, -80.5818245]
-    map = L.mapbox.map('map', 'mapbox.streets').setView([43.4822754, -80.5818245], zoomLevel);
+    map = L.mapbox.map('map', 'mapbox.streets', {
+        maxZoom: 18,
+        minZoom: 14
+    });
+    //.setView([43.4822754, -80.5818245], zoomLevel);
     getCurrentLocation();
     var string = navigator.userAgent;
     map.on('locationfound', onLocationFound);
-        map.on('click', onMapClick);
-
+    map.on('click', onMapClick);
+    settingCityBoundary(null);
+    maskingMap();
 }]);
+
+//current City Coords are limited to waterloo (the cityCoords is not being populated)
+function settingCityBoundary(cityCoords) {
+    // Construct a bounding box for this map that the user cannot
+    // move out of
+
+    var southWest = L.latLng(43.432744, -80.574186),
+        northEast = L.latLng(43.532264, -80.479944),
+        bounds = L.latLngBounds(southWest, northEast);
+
+    map.setMaxBounds(bounds);
+    //   map = L.mapbox.map('map', 'mapbox.streets', {
+    //               // set that bounding box as maxBounds to restrict moving the map
+    //               // see full maxBounds documentation:
+    //               // http://leafletjs.com/reference.html#map-maxbounds
+    //               maxBounds: bounds,
+    //               maxZoom: 19,
+    //               minZoom: 10
+    //    });
+
+    // zoom the map to that bounding box
+    //    map.fitBounds(bounds);
+}
 
 function displayMap(position) {
     map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
 };
 
 
-function onMapClick(e){
+function maskingMap(){
+    // Create background tile layer
+//  var bg = L.tileLayer(...);
+    var bg = L.tileLayer(this);
+
+  // Create foreground tile layer with mask
+//                      http://www.finds.jp/ws/tmc/1.0.0/Kanto_Rapid-900913-L/{z}/{x}/{y}.png
+
+  var fg = L.tileLayer.mask('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {
+//          maskUrl : 'star.png', // optional
+//          maskWidth : 800,  //optional
+//          maskHeight : 800 //optional
+      });
+
+  // Create map with layers and bind listener to update center of mask
+
+//    map.
+
+    L.control.layers(bg, fg).addTo(this.map);
+        this.map.on("mousemove", function(e) {
+      fg.setCenter(e.containerPoint.x, e.containerPoint.y);
+   });
+//    map = L.map("map", {
+////              zoom : ...,
+////      center : ...,
+//      layers : [ bg, fg ]
+//   })
+
+};
+
+
+function onMapClick(e) {
     L.marker(e.latlng
-//             , {
-//            icon: L.icon({
-//                iconUrl: 'leaf-green.png',
-//                iconSize: [38, 95], // size of the icon
-//                iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-//                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-//            })
-//        }
-            ).addTo(map)
+            //             , {
+            //            icon: L.icon({
+            //                iconUrl: 'leaf-green.png',
+            //                iconSize: [38, 95], // size of the icon
+            //                iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            //                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            //            })
+            //        }
+        ).addTo(map)
         .bindPopup(formHTML).openPopup();
 
-//    alert("You clicked at "+e.latlng);
+    //    alert("You clicked at "+e.latlng);
 }
 
 function getCurrentLocation() {
@@ -68,13 +126,13 @@ function onLocationFound(e) {
     getJson(e);
     // we will not display a marker for every movement that the user makes
 
-    setTimeout(getCurrentLocation, 1000);
+    //setTimeout(getCurrentLocation, 1000);
 };
 //get the Json from backend server
 function getJson(e) {
     var discoverApii = "http://172.31.11.163/findingwaterloo/index.php/api/things"
     info = $.getJSON(discoverApii, function () {
-            console.log("success on access command");
+            //            console.log("success on access command");
         })
         //do when success
         .done(function (data) {
@@ -93,7 +151,7 @@ function getJson(e) {
                 created = data[i].tgh_created_at;
                 icon = data[i].tty_icon;
 
-                console.log(title + "disc   " + discription + "addr  " + address + "create    " + created + "  icon " + icon);
+                //                console.log(title + "disc   " + discription + "addr  " + address + "create    " + created + "  icon " + icon);
 
                 var radius = e.accuracy / 2;
 
@@ -101,28 +159,28 @@ function getJson(e) {
                 //shoot the windows on the map
                 L.marker(e.latlng
 
-                        , {
-                            icon: L.icon({
-                                //iconUrl: 'leaf-green.png',
-                                iconUrl: icon,
-                                iconSize: [38, 95], // size of the icon
-                                iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-                                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-
-                            })
-                        }
-                    ).addTo(map)
-                    .bindPopup("Name: " + title + "<br>" + "Description" + discription + "<br>" + "Address " + address + "<br>" + "Created " + created).openPopup();
+                    //                        , {
+                    //                            icon: L.icon({
+                    //                                //iconUrl: 'leaf-green.png',
+                    //                                iconUrl: icon,
+                    //                                iconSize: [38, 95], // size of the icon
+                    //                                iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+                    //                                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+                    //
+                    //                            })
+                    //                        }
+                ).addTo(map);
+                //.bindPopup("Name: " + title + "<br>" + "Description" + discription + "<br>" + "Address " + address + "<br>" + "Created " + created).openPopup();
                 L.circle(e.latlng, radius).addTo(map);
 
                 //return[title, discription, address, created];
 
             }
 
-            console.log("second success");
+            //            console.log("second success");
         })
         // do when the connection failed
         .fail(function () {
-            console.log("error");
+            //            console.log("error");
         })
 };
