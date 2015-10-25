31b51daf297c1dd58960ee798f778587d6387d1c
Bobin Cherian <bobinbc@gmail.com>
2015-10-24 21:26:13 -0400

    Calling get location every second.

--- app/controllers/mapViewController.js
+++ app/controllers/mapViewController.js
@@ -4,29 +4,14 @@ var zoomLevel = 13;
 'use strict';
 app.controller('mapViewController', ['$scope', function ($scope) {
 
-     L.mapbox.accessToken = 'pk.eyJ1IjoiYm9iaW5iYyIsImEiOiJ2MkpyLTMwIn0.nay6_wKcFJpEC1y2Y6PDCw';
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYm9iaW5iYyIsImEiOiJ2MkpyLTMwIn0.nay6_wKcFJpEC1y2Y6PDCw';
     //Waterloo coordindates - [43.4822754, -80.5818245]
-    map = L.mapbox.map('map', 'mapbox.streets')
-            .setView([43.4822754, -80.5818245], zoomLevel);
-
-
-       getCurrentLocation();
+    map = L.mapbox.map('map', 'mapbox.streets').setView([43.4822754, -80.5818245], zoomLevel);
+    getCurrentLocation();
     var string = navigator.userAgent;
     map.on('locationfound', onLocationFound);
 
 }]);
-
-
-//function Initialize() {
-//
-//
-//
-//
-//    getCurrentLocation();
-//    var string = navigator.userAgent;
-//    map.on('locationfound', onLocationFound);
-//
-//};
 
 function displayMap(position) {
     map.panTo(new L.LatLng(position.coords.latitude, position.coords.longitude));
@@ -34,45 +19,36 @@ function displayMap(position) {
 
 function getCurrentLocation() {
 
-    map.locate({setView: true, maxZoom: 16});
+    map.locate({
+        setView: true,
+        maxZoom: 16
+    });
 
 
-//    if (navigator.geolocation) {
-//        navigator.geolocation.getCurrentPosition(displayMap);
-//    } else {
-//        x.innerHTML = "Geolocation is not supported by this browser.";
-//    }
+    //    if (navigator.geolocation) {
+    //        navigator.geolocation.getCurrentPosition(displayMap);
+    //    } else {
+    //        x.innerHTML = "Geolocation is not supported by this browser.";
+    //    }
 };
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
-    L.marker(e.latlng, {icon: L.icon({
-        iconUrl: 'leaf-green.png',
-        iconSize: [38, 95], // size of the icon
-        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    // we will not display a marker for every movement that the user makes
+    L.marker(e.latlng
 
-    })} ).addTo(map)
+            //             , {icon: L.icon({
+            //        iconUrl: 'leaf-green.png',
+            //        iconSize: [38, 95], // size of the icon
+            //        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            //        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            //
+            //    })}
+        ).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
     L.circle(e.latlng, radius).addTo(map);
+
+    setTimeout(getCurrentLocation, 1000);
 };
-
-
-
-
-
-function createIcon() {
-
-    var greenIcon = L.icon({
-        iconUrl: 'leaf-green.png',
-
-        iconSize: [38, 95], // size of the icon
-        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62], // the same for the shadow
-        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-
-    })
-};
-window.onload = Initialize;
