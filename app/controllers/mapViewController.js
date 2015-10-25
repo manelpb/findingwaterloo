6a6f837c325edc8ea80d3b260ce7b654b4c56591
Kevin <kevin321an@gmail.com>
2015-10-24 22:17:53 -0400

    move the functions to new palce

--- app/controllers/mapViewController.js
+++ app/controllers/mapViewController.js
@@ -34,9 +34,43 @@ function getCurrentLocation() {
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
-
+    getJson(e);
     // we will not display a marker for every movement that the user makes
-    L.marker(e.latlng
+
+
+
+    setTimeout(getCurrentLocation, 1000);
+};
+//get the Json from backend server
+function getJson(e){
+    var discoverApii = "http://172.31.11.163/findingwaterloo/index.php/api/things"
+    info = $.getJSON( discoverApii, function() {
+      console.log( "success on access command" );
+    })
+        //do when success
+     .done(function(data) {
+        //console.log(data);
+        for (var i in data) {
+            //output2 += "<li>" + data[i].thgh_geo.location.lat"</li>";
+            lati = (data[i]. tgh_geo. location.lat);
+            longti = (data[i]. tgh_geo. location.lng);
+            /*if (lati+INTERVAL>e.latitude || lati-INTERVAL<lat)&&(longti+INTERVAAL>e.longitude || longti < e.longitude )
+                {
+
+                }*/
+            title = data[i].thg_title;
+            discription= data[i].tgh_description;
+            address = data[i].tgh_address;
+            created = data[i].tgh_created_at;
+            icon = data[i].tty_icon;
+
+            console.log(title+"disc   "+discription+"addr  "+address+"create    "+created+"  icon "+icon);
+
+            var radius = e.accuracy / 2;
+
+
+            //shoot the windows on the map
+            L.marker(e.latlng
 
             //             , {icon: L.icon({
             //        iconUrl: 'leaf-green.png',
@@ -46,9 +80,22 @@ function onLocationFound(e) {
             //
             //    })}
         ).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        .bindPopup("Name: " + title +"<br>" + "Descripstion" + discription + "<br>"+"Address "+ address + "<br>"+"Created " + created).openPopup();
+        L.circle(e.latlng, radius).addTo(map);
 
-    L.circle(e.latlng, radius).addTo(map);
 
-    setTimeout(getCurrentLocation, 1000);
+            //return[title, discription, address, created];
+
+        }
+
+        console.log( "second success" );
+      })
+    // do when the connection failed
+      .fail(function() {
+        console.log( "error" );
+      })
+
+
+
+
 };
